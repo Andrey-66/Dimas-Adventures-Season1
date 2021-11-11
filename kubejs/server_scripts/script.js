94b557934d9bded55f95d775d34df1b754547139
Andrey-66 <andrey66-66-66@yandex.ru>
2021-11-11 22:22:06 +0300

    1

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -8,7 +8,26 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-
+		event.remove({output: 'minecraft:magma_block'})
+		event.shaped('3x minecraft:stone', [
+		  'SAS',
+		  'S S',
+		  'SAS'
+		], {
+		  S: 'minecraft:sponge',
+		  A: 'minecraft:apple'
+		})
+		event.custom({
+			type: 'create:crushing',
+			ingredients: [
+			  Ingredient.of('botania:terrasteel_ingot').toJson()
+			],
+			results: [
+			  Item.of('3x botania:fertilizer').toResultJson(),
+			  Item.of('4x botania:terrasteel_nugget').withChance(1).toResultJson()
+			],
+			processingTime: 100
+		  })
 })
 
 onEvent('item.tags', event => {
