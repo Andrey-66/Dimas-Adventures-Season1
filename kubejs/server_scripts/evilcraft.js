3e32319165edba7d696e08573f7ce7727e693a0b
Andrey-66 <andrey66-66-66@yandex.ru>
2021-11-21 03:20:26 +0300

    botania quest done

--- kubejs/server_scripts/evilcraft.js
+++ kubejs/server_scripts/evilcraft.js
@@ -1,6 +1,6 @@
 onEvent('recipes', event => {
     event.remove({output: 'evilcraft:blood_extractor'})
-    event.remove({output: 'evilcraft:blood_infusion_core'})
+
     event.remove({output: 'evilcraft:blood_infuser'})
     event.shaped('evilcraft:blood_extractor', [
       'III',
@@ -14,6 +14,7 @@ onEvent('recipes', event => {
       G: '#forge:glass'
     })
 
+    event.remove({output: 'evilcraft:blood_infusion_core'})
     event.shaped('evilcraft:blood_infusion_core', [
       'KKK',
       'KZK',
@@ -21,5 +22,17 @@ onEvent('recipes', event => {
       ], {
       K: 'evilcraft:hardened_blood_shard',
       Z: 'botania:spark'
+    })
+    
+    event.remove({output: Item.of('evilcraft:box_of_eternal_closure', '{spiritTag:{playerName:"",remainingLife:0,isSwarm:1b,swarmTier:2,buildupDuration:0,frozenDuration:0,playerId:""}}')})
+    event.shaped(Item.of('evilcraft:box_of_eternal_closure', '{spiritTag:{playerName:"",remainingLife:0,isSwarm:1b,swarmTier:2,buildupDuration:0,frozenDuration:0,playerId:""}}'), [
+      'PPP',
+      'BSI',
+      'PPP'
+      ], {
+      P: 'create:powdered_obsidian',
+      B: Item.of('botania:brew_flask', '{brewKey:"botania:clear"}'),
+      S: 'minecraft:ender_chest',
+      I: Item.of('botania:brew_flask', '{brewKey:"botania:bloodthirst"}')
     })
 })
