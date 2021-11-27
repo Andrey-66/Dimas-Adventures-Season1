9402f3b023c97ea1ac01ceb2c804e1dbc45520a9
Niketats <torianet103@gmail.com>
2021-11-27 15:52:27 +0300

    Fixed BetterEnd missed #minecraft:dragon_immune block tag
    Added #minecraft:dragon_immune to
    'betterendforge:thallasium_bars'
    'betterendforge:flavolite'
    'betterendforge:flavolite_slab'
    'betterendforge:flavolite_stairs'
    'betterendforge:flavolite_wall'

---
+++ client_folder/kubejs/server_scripts/betterendtags.js
@@ -0,0 +1,7 @@
+onEvent('block.tags', event => {
+  event.add('minecraft:dragon_immune', 'betterendforge:flavolite')
+  event.add('minecraft:dragon_immune', 'betterendforge:flavolite_stairs')
+  event.add('minecraft:dragon_immune', 'betterendforge:flavolite_slab')
+  event.add('minecraft:dragon_immune', 'betterendforge:flavolite_wall')
+  event.add('minecraft:dragon_immune', 'betterendforge:thallasium_bars')
+  })
