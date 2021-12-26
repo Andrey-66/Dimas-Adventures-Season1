75ccaab8462f35891e9f9456e7abca4df30596e1
Niketats <torianet103@gmail.com>
2021-12-26 03:44:41 +0300

    fix create recipes

---
+++ server_folder/kubejs/server_scripts/recipefix.js
@@ -0,0 +1,8 @@
+onEvent('recipes', event => {
+  event.remove({id: 'powah:energizing/energized_steel'})
+  event.remove({id: 'powah:energizing/blazing_crystal'})
+  event.remove({id: 'powah:energizing/blazing_crystal_2'})
+  event.remove({id: 'powah:energizing/niotic_crystal'})
+  event.remove({id: 'powah:energizing/spirited_crystal'})
+  event.remove({id: 'powah:energizing/nitro_crystal'})
+})
